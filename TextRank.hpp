188200ad2edc47d9c485c53bdb966495c467dc55
Pavlo Shvaiko <shvaikop02@gmail.com>
2023-04-30 16:18:49 +0200

    Added exceptions and comments to TextRank

--- TextRank.hpp
+++ TextRank.hpp
@@ -11,7 +11,6 @@ struct TextRank_Node {
     double norm_constant;
     size_t sent_index;
     std::vector< std::pair<size_t, double> > edges;
-
 };
 
 class TextRank {
@@ -30,23 +29,31 @@ public:
     }
 
     strVec get_summary(double percent = (double)1 / 3) {
-        // TODO throw exception in percent is not in [0,1]
+        if (percent < 0 || percent > 1) {
+            throw std::runtime_error("Error: Percentage of sentences included in the summary should be between 0 and 1!");
+        }
         size_t len = graph_.size() * percent;
         return get_summary_priv(len);
     }
 
     strVec get_summary(size_t len) {
-        // TODO issue a warning if len is greater than the number of sentences
+        if (len < 0) {
+            throw std::runtime_error("Error: Length of summary cannot be negative!");
+        }
+        if (len > sentences_.size()) {
+            std::cerr << "Warning: Length of summary requested is longer than the text." << std::endl;
+        }
         len = std::min(len, sentences_.size());
         return get_summary_priv(len);
     }
 
 private:
-
-    static bool doublesEqual(double a, double b, double epsilon) {
+    // Equality for doubles, epsilon defines precision required
+    static bool doublesEqual(double a, double b, double epsilon = 1e-9) {
         return std::abs(a - b) < epsilon;
     }
 
+    // Calculates the similarity between 2 sentences
     static double similarity(strVec& sent_1, strVec& sent_2) {
         double top = 0;
         for (auto& word : sent_1) {
@@ -58,6 +65,8 @@ private:
         return top / bottom;
     }
 
+    // Calculates the normalization constants for each node
+    // Normalization Constant = Sum of the weights of all outgoing edges
     void set_norm_constants() {
         for (TextRank_Node& node: graph_) {
             double sum = 0;
@@ -75,14 +84,14 @@ private:
         for (size_t i = 0; i < size; i++) {
             graph_.emplace_back();
             graph_[i].sent_index = i;
-            graph_[i].norm_constant = 1;
+            graph_[i].norm_constant = 1;    // default normalization constant
             graph_[i].score = init_score;
         }
 
         for (size_t i = 0; i < size; i++) {
             for (size_t j = i + 1; j < size; j++) {
                 double sim_i_j = similarity(tokenized_sentences_[i], tokenized_sentences_[j]);
-                if (!doublesEqual(sim_i_j, 0, 1e-9)) {
+                if (!doublesEqual(sim_i_j, 0)) {
                     graph_[i].edges.emplace_back(j, sim_i_j);
                     graph_[j].edges.emplace_back(i, sim_i_j);
                 }
@@ -91,6 +100,8 @@ private:
         set_norm_constants();
     }
 
+    // A single iteration of the algorithm that incrementally updates node scores
+    // Returns the total change in scores during this iteration
     double iteration(double d) {
         double change = 0;
         for (TextRank_Node& node : graph_) {
@@ -109,6 +120,8 @@ private:
         return change;
     }
 
+    // Comparison function for Nodes to be used by std::set and std::sort
+    // First compares scores, then indices
     static bool custom_comp (const TextRank_Node& a, const TextRank_Node& b) {
         if (a.score != b.score) {
             return a.score > b.score;
@@ -116,6 +129,7 @@ private:
         return a.sent_index < b.sent_index;
     }
 
+    // Iterates until scores reach equilibrium
     void iterate() {
         double change = std::numeric_limits<double>::max();
         while (change > 0.001) {
@@ -123,14 +137,16 @@ private:
         }
     }
 
+    // Returns summary of specified number of sentences
     strVec get_summary_priv(size_t len) {
         iterate();
         std::sort(graph_.begin(), graph_.end(), custom_comp);   // sort graph_ by scores
-        std::vector<size_t> summary_sents_i;
+        std::vector<size_t> summary_sents_i;    // sentence indices
         summary_sents_i.reserve(len);
-        for (size_t i = 0; i < len; i++) {
+        for (size_t i = 0; i < len; i++) {      // store indices of sentences that should be in the summary
             summary_sents_i.push_back(graph_[i].sent_index);
         }
+        // sort the indices so that sentences in the summary appear in order
         std::sort(summary_sents_i.begin(), summary_sents_i.end());
 
         strVec summary;
